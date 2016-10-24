bbc0908ace80638e6eb0144cad490d7c2c82f1f8
spurge <olof@klandestino.se>
2016-10-24 15:10:36 +0200

    Rewrote messenger rabbitmq handler with exchanges and declares

--- messenger.py
+++ messenger.py
@@ -3,15 +3,19 @@
 import json
 import pika
 
+from functools import partial
 from hashlib import md5
 from os import uname
 from rx import Observable
-from rx.concurrency import AsyncIOScheduler
 from time import time
-from uuid import getnode
+from uuid import uuid1
 
 
 class Messenger:
+
+    COMMAND_QUEUE_NAME = 'clique-command'
+    STATUS_EXCHANGE_NAME = 'clique-status'
+    STATUS_QUEUE_NAME = 'clique-status-%s'
 
     def __init__(self, url):
         self.url = url
@@ -22,7 +26,7 @@ class Messenger:
     @property
     def uuid(self):
         if self.__uuid is None:
-            self.__uuid = str(getnode())
+            self.__uuid = str(uuid1())
 
         return self.__uuid
 
@@ -40,11 +44,31 @@ class Messenger:
     def channel(self):
         if self.__channel is None:
             self.__channel = self.connection.channel()
-            self.__channel.queue_declare(queue='clique-command')
             self.__channel.queue_declare(queue='clique-response')
-            self.__channel.queue_declare(queue='clique-status')
 
         return self.__channel
+
+    @property
+    def command_queue(self):
+        self.channel.queue_declare(queue=self.COMMAND_QUEUE_NAME)
+        return self.COMMAND_QUEUE_NAME
+
+    @property
+    def status_exchange(self):
+        self.channel.exchange_declare(exchange=self.STATUS_EXCHANGE_NAME,
+                                      exchange_type='fanout')
+        return self.STATUS_EXCHANGE_NAME
+
+    @property
+    def status_queue(self):
+        queue = self.channel.queue_declare(
+            queue=self.STATUS_QUEUE_NAME % self.uuid,
+            exclusive=True
+        )
+        self.channel.queue_bind(exchange=self.status_exchange,
+                                routing_key='',
+                                queue=queue.method.queue)
+        return queue.method.queue
 
     def encode_message(self, **kwargs):
         props = dict(uuid=self.uuid,
@@ -62,38 +86,66 @@ class Messenger:
         return checksum
 
     def publish_online(self):
-        return self.publish('status', uname=uname())
+        return self.publish_stats(uname=uname())
 
     def publish_command(self, command, **kwargs):
-        return self.publish('command', command=command, **kwargs)
+        checksum, message = self.encode_message(command=command, **kwargs)
+        self.channel.basic_publish(exchange='',
+                                   routing_key=self.command_queue,
+                                   body=message)
+        return checksum
 
     def publish_response(self, command, **kwargs):
         return self.publish('response', command=command, **kwargs)
 
-    def publish_stats(self, stats):
-        return self.publish('status', stats=stats)
+    def publish_stats(self, **stats):
+        checksum, message = self.encode_message(**stats)
+        self.channel.basic_publish(exchange=self.status_exchange,
+                                   routing_key='',
+                                   body=message)
+        return checksum
 
-    def fetch(self, key):
+    def ack_message(self, tag):
+        self.channel.basic_ack(tag)
+
+    def decode_message(self, callback, channel, method, properties, body):
         try:
-            m, p, b = self.channel.basic_get(queue='clique-%s' % key)
-
-            if b is not None:
-                self.channel.basic_ack(m.delivery_tag)
-                return json.loads(b.decode('utf8'))
+            callback((method.delivery_tag,
+                      json.loads(body.decode('utf8'))))
         except:
-            pass
+            self.ack_message(method.delivery_tag)
+
+    def create_listener(self, kwargs, observer):
+        try:
+            self.channel.basic_consume(partial(self.decode_message,
+                                               observer.on_next),
+                                       **kwargs)
+            self.channel.basic_qos(prefetch_count=1)
+            self.channel.start_consuming()
+        except Exception as error:
+            self.channel.stop_consuming()
+            observer.on_error(error)
+
+        observer.on_completed()
 
     def get_listener(self, key):
-        return Observable.interval(1000,
-                                   scheduler=AsyncIOScheduler()) \
-            .map(lambda _: self.fetch(key)) \
-            .where(lambda c: c is not None)
+        return Observable.create(partial(self.create_listener,
+                                         dict(queue='clique-%s' % key))) \
+            .where(lambda m: m is not None)
 
     def get_status_listener(self):
-        return self.get_listener('status')
+        return Observable.create(partial(self.create_listener,
+                                         dict(queue=self.status_queue))) \
+            .where(lambda m: m is not None) \
+            .tap(lambda r: self.ack_message(r[0]))
 
     def get_command_listener(self):
-        return self.get_listener('command')
+        return Observable.create(partial(self.create_listener,
+                                         dict(queue=self.command_queue))) \
+            .where(lambda m: m is not None)
 
     def get_response_listener(self):
         return self.get_listener('response')
+
+    def stop_listeners(self):
+        self.channel.stop_consuming()
--- test/messenger.py
+++ test/messenger.py
@@ -2,9 +2,19 @@
 
 import asyncio
 
+from functools import partial
 from unittest import TestCase
 
 from messenger import Messenger
+
+
+def stop(messenger, _):
+    messenger.stop_listeners()
+
+
+def error(messenger, error):
+    messenger.stop_listeners()
+    raise error
 
 
 class TestMessenger(TestCase):
@@ -19,32 +29,47 @@ class TestMessenger(TestCase):
     def test_online(self):
         loop = asyncio.get_event_loop()
 
-        result = loop.run_until_complete(
+        self.assertEqual(self.messenger.status_queue,
+                         Messenger.STATUS_QUEUE_NAME % self.messenger.uuid)
+        self.assertIsInstance(self.messenger.publish_online(), str)
+
+        tag, status = loop.run_until_complete(
             self.messenger
-            .get_status_listener()
-            .first()
-            .timeout(3000)
+                .get_status_listener()
+                .where(lambda r: r[1]['uuid'] == self.messenger.uuid)
+                .tap(partial(stop, self.messenger))
+                .timeout(3000)
+                .catch_exception(partial(error, self.messenger))
         )
 
-        self.assertIsInstance(result['uname'], list)
-        self.assertIsInstance(result['time'], float)
-        self.assertIsInstance(result['checksum'], str)
-        self.assertEqual(result['uuid'], self.messenger.uuid)
+        self.assertIsInstance(tag, int)
+        self.assertIsInstance(status['uname'], list)
+        self.assertIsInstance(status['time'], float)
+        self.assertIsInstance(status['checksum'], str)
+        self.assertEqual(status['uuid'], self.messenger.uuid)
 
     def test_command(self):
         checksum = self.messenger.publish_command('test',
                                                   arg='value')
         loop = asyncio.get_event_loop()
 
-        result = loop.run_until_complete(
+        tag, command = loop.run_until_complete(
             self.messenger
             .get_command_listener()
-            .where(lambda c: c['checksum'] == checksum)
-            .first()
+            .where(
+                lambda c:
+                    c[1]['checksum'] == checksum and
+                    c[1]['command'] == 'test'
+            )
+            .tap(partial(stop, self.messenger))
             .timeout(3000)
+            .catch_exception(partial(error, self.messenger))
         )
 
-        self.assertEqual(result['command'], 'test')
-        self.assertEqual(result['arg'], 'value')
-        self.assertEqual(result['uuid'], self.messenger.uuid)
-        self.assertIsInstance(result['time'], float)
+        self.assertIsInstance(tag, int)
+        self.messenger.ack_message(tag)
+
+        self.assertEqual(command['command'], 'test')
+        self.assertEqual(command['arg'], 'value')
+        self.assertEqual(command['uuid'], self.messenger.uuid)
+        self.assertIsInstance(command['time'], float)
