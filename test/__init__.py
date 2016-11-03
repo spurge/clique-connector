0d1813539b067b0157a60ef1c773df0dd46150d4
spurge <olof@klandestino.se>
2016-11-03 12:51:00 +0100

    Fixed test imports

---
+++ test/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2016 Olof Montin <olof@montin.net>
+
+This file is part of clique-connector.
+"""
+
+from .connector import TestConnector
+from .messenger import TestMessenger
--- test/messenger.py
+++ test/messenger.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+"""
+Copyright (c) 2016 Olof Montin <olof@montin.net>
+
+This file is part of clique-connector.
+"""
+
 import asyncio
 import logging
 
